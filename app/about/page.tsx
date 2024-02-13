7fa84f51f0fff8caae3c9ddc4f55af0942aa63f1
Endale2 <endale406@gmail.com>
2024-02-13 00:52:48 -0800

    project added

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About(){
+    return (
+        <div className="">
+            <h1>About me</h1>
+            <Link href="/" >Home page</Link>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>my first  tutorial</h1>
+      <Link href="/about" >abou page</Link>
+    </div>
+  );
+}
